24dbeb83c0173d475635841aee8bacf990c3c382
Salman-rahi <95650802+Salman-rahi@users.noreply.github.com>
2024-10-22 06:59:35 -0400

    add data'

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './Componets/Blogs/Blogs'
+import Header from './Componets/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
-      <h1 className='text-6xl'>Knowladge cafe</h1>
-
+    <Header></Header>
+    <Blogs></Blogs>
 
     </>
   )
